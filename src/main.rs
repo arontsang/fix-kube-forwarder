edff9413478d8210a97d6606632ca3d9a13a5356
Aron Tsang <arontsang@gmail.com>
2025-01-12 17:54:41 +0800

    Add code to connect to acceptor

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-use futures_lite::io::AsyncReadExt;
+
 use futures_lite::FutureExt;
-use futures_lite::{AsyncWriteExt};
 use glommio::net::{TcpListener, TcpStream};
-use glommio::{timer::sleep, LocalExecutor};
+use glommio::{GlommioError, LocalExecutor};
 use reinterpret::reinterpret_mut_slice;
-use ringbuf::storage::Heap;
-use ringbuf::HeapRb;
-use ringbuf::{consumer::Consumer, producer::Producer, traits::Observer, SharedRb};
-use std::io::{Error, IoSliceMut, Result};
-use std::{mem, str};
-use std::time::Duration;
+use std::io::{Error, Result};
+use std::str;
+use futures::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 use twoway::find_bytes;
 
 
@@ -18,97 +14,96 @@ const FIX_CHECKSUM_TAG: &[u8; 4] = b"\x0110=";
 async fn main_async() -> Result<()> {
     let ex = async_executor::LocalExecutor::new();
 
-    let foo = async {
+    let accept_client_task = async {
         let listener = TcpListener::bind("0.0.0.0:10000")?;
         loop {
-            let client = listener.accept().await?;
+            let Ok(mut client) = listener.accept().await else { break; };
             ex.spawn(async move {
-                let mut client = client;
-                let mut read_buffer = HeapRb::<u8>::new({8 * 1024});
-                loop {
-                    read_to_buffer(&mut client, &mut read_buffer).await?;
-
-                    println!("{}", read_buffer.occupied_len());
-
-                    let (packet, _) = read_buffer.as_slices();
-
-                    let Some(index) = find_bytes(packet, FIX_CHECKSUM_TAG) else { continue };
-                    let index_end_msg = {
-                        let tail = &packet[index..][size_of_val(FIX_CHECKSUM_TAG)..];
-                        tail.iter().position(|&x| x == 0x01)
-                    };
-                    let Some(index_end_msg) = index_end_msg else { continue };
-                    let index_end_msg = index_end_msg + index + size_of_val(FIX_CHECKSUM_TAG) + 1;
-
-                    let log_on_message = &packet[0..index_end_msg];
-                    unsafe { read_buffer.advance_read_index(index) }
-
-                    let target_comp_id = {
-                        const TARGET_COMP_ID_PREAMBLE: &[u8; 4] = b"\x0156=";
-                        let comp_id_start_position = find_bytes(log_on_message, TARGET_COMP_ID_PREAMBLE).unwrap();
-                        let tail = &log_on_message[comp_id_start_position..][size_of_val(TARGET_COMP_ID_PREAMBLE)..];
-                        let end_comp_id = tail.iter().position(|&x| x == 0x01).unwrap();
-                        let comp_id = &tail[0..end_comp_id];
-                        let comp_id = str::from_utf8(comp_id).unwrap();
-                        comp_id
-                    };
-
-                    println!("{}", target_comp_id);
-                }
-
-                Ok::<(), Error>(())
-            }).detach()
+                handle_client(& mut client).await }
+            ).detach()
         }
-
         Ok::<(), Error>(())
     };
 
-    // let qux = async move{
-    //     let client = TcpStream::connect("127.0.0.1:10000").await?;
-    //     let mut client = client.buffered();
-    //
-    //
-    //     let buf = [0u8; 512];
-    //     client.write(&buf).await?;
-    //     loop {
-    //         let buf = [255u8; 1024];
-    //         client.write(&buf).await?;
-    //         sleep(Duration::from_millis(100)).await;
-    //     }
-    // };
+    let handle_client_task = async {
+        loop {
+            ex.tick().await
+        }
+    };
 
-    foo
-        //.or(qux)
-        .or(async { loop { ex.tick().await } }).await?;
+    accept_client_task
+        .or(handle_client_task).await?;
 
     Ok(())
 }
 
-async fn read_to_buffer(stream: &mut TcpStream, read_buffer: &mut SharedRb<Heap<u8>>) -> Result<()> {
-    unsafe {
-        let foo = read_buffer.vacant_slices_mut();
-        //let foo: &mut [u8] = reinterpret_mut_slice(foo);
-        //let bar: &mut [u8] = reinterpret_mut_slice(bar);
+async fn handle_client(client: &mut TcpStream) {
+    let mut read_buffer = Vec::<u8>::with_capacity(1024);
+    let work: core::result::Result<(), Error> = {
+        loop {
+            unsafe  {
+                let buf = read_buffer.spare_capacity_mut();
+                let buf: &mut [u8] = reinterpret_mut_slice(buf);
+                let Ok(read_size) = client.read(buf).await else { break };
+                if read_size == 0 { break; }
 
+                read_buffer.set_len(read_buffer.len() + read_size);
+            };
+            let packet = read_buffer.as_slice();
 
+            let Some(index) = find_bytes(packet, FIX_CHECKSUM_TAG) else { continue };
+            let index_end_msg = {
+                let tail = &packet[index..][size_of_val(FIX_CHECKSUM_TAG)..];
+                tail.iter().position(|&x| x == 0x01)
+            };
+            let Some(index_end_msg) = index_end_msg else { continue };
+            let index_end_msg = index_end_msg + index + size_of_val(FIX_CHECKSUM_TAG) + 1;
 
-        let bar = reinterpret_mut_slice(foo.0);
-        let baz = reinterpret_mut_slice(foo.1);
+            let log_on_message = &packet[0..index_end_msg];
+            let target_comp_id = {
+                const TARGET_COMP_ID_PREAMBLE: &[u8; 4] = b"\x0156=";
+                let comp_id_start_position = find_bytes(log_on_message, TARGET_COMP_ID_PREAMBLE).unwrap();
+                let tail = &log_on_message[comp_id_start_position..][size_of_val(TARGET_COMP_ID_PREAMBLE)..];
+                let end_comp_id = tail.iter().position(|&x| x == 0x01).unwrap();
+                let comp_id = &tail[0..end_comp_id];
+                let comp_id = str::from_utf8(comp_id).unwrap();
+                comp_id
+            };
 
-        bar.fill(0);
-        baz.fill(0);
+            let Ok(mut fix_acceptor) = get_proxy_target(target_comp_id).await else { break; };
 
-        let advance = {
-            let bar = IoSliceMut::new(bar);
-            let baz = IoSliceMut::new(baz);
+            async fn copy_stream<TRead: AsyncRead + Unpin, TWrite: AsyncWrite + Unpin>(mut source: TRead, mut target: TWrite) {
+                let mut buffer = [0u8; 1024 * 8];
+                let buffer = &mut buffer;
+                loop {
+                    let Ok(read_size) = source.read(buffer).await else { break };
+                    if read_size == 0 { break; }
 
-            println!("Bar is {} sized", bar.len());
+                    let buffer = &buffer[..read_size];
+                    if !target.write(buffer).await.is_ok() {
+                        break;
+                    }
+                }
+            }
+            
+            fix_acceptor.write(packet).await.expect("TODO: panic message");
 
-            stream.read_vectored(&mut [bar, baz]).await?
+            let (client_read, client_write) = client.split();
+            let (acceptor_read, acceptor_write) = fix_acceptor.split();
+
+            let copy_to_acceptor = copy_stream(client_read, acceptor_write);
+            let copy_from_acceptor = copy_stream(acceptor_read, client_write);
+
+            copy_from_acceptor.or(copy_to_acceptor).await
         };
-        read_buffer.advance_write_index(advance);
+        Ok(())
     };
-    Ok(())
+
+    work.unwrap_or_default()
+}
+
+async fn get_proxy_target(_target_comp_id: &str) -> std::result::Result<TcpStream, GlommioError<()>> {
+    TcpStream::connect("172.20.8.3:9880").await
 }
 
 fn main() -> Result<()> {
